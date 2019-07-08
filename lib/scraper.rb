ffb0502f5c8d9e7069289487f10625f7f7ce81bf
George Ruiz <georuiz817@gmail.com>
2019-07-08 01:18:44 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,6 +5,8 @@ class Scraper
 
   def self.scrape_index_page(index_url)
    index.html = open(index_url)
+   index_doc = Nokogiri::HTML(index_html)
+   
   end
 
   def self.scrape_profile_page(profile_url)
