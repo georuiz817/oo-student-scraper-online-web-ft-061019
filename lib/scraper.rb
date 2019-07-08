1ef4b40bda91bd1d42f3f28933eba485ca3ad337
George Ruiz <georuiz817@gmail.com>
2019-07-08 01:24:24 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,7 +4,7 @@ require 'nokogiri'
 class Scraper
 
   def self.scrape_index_page(index_url)
- doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
+ doc = Nokogiri::HTML(open("./fixtures/student-site/index.html"))
   end
     	  
   
