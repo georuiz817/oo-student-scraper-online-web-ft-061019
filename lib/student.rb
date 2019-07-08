65618a491ef4dae2c701c6fb4852785f932de904
George Ruiz <georuiz817@gmail.com>
2019-07-08 01:56:39 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -6,11 +6,9 @@ class Student
 
   def initialize(student_hash)
   self.send("name=", student_hash[:name])
-  
-    self.send("location=", student_hash[:location])
-  ]
-    self.send("profile_url=", student_hash[:profile_url])
-    @@all << self
+  self.send("location=", student_hash[:location])
+  self.send("profile_url=", student_hash[:profile_url])
+  @@all << self
   end
 
   def self.create_from_collection(students_array)
