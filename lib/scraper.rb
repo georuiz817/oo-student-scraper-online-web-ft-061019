f439dc4773aa3d18028c524ae38e1f4867f9d286
George Ruiz <georuiz817@gmail.com>
2019-07-08 01:13:06 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,11 +1,12 @@
 require 'open-uri'
 require 'pry'
-
+require 'Nokogiri'
 class Scraper
 
   def self.scrape_index_page(index_url)
    students_hash = []
-    html = Nokogiri::HTML(open("./fixtures/student-site/index.html"))
+   html = Nokogiri::HTML(open("./fixtures/student-site/index.html"))
+   binding.pry
   end
 
   def self.scrape_profile_page(profile_url)
