516ae5b44cce38a8971127f6caa011f460e75a2e
George Ruiz <georuiz817@gmail.com>
2019-07-08 01:09:52 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,12 +1,10 @@
 require 'open-uri'
 require 'pry'
-require 'nokogiri'
+
 class Scraper
 
   def self.scrape_index_page(index_url)
-  html = File.read('fixtures/kickstarter.html') 
- 
-      binding.pry
+  
   end
 
   def self.scrape_profile_page(profile_url)
