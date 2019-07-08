028abce716a56005b3bd2e2448a6be768c663272
George Ruiz <georuiz817@gmail.com>
2019-07-08 02:00:58 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -7,23 +7,23 @@ class Scraper
     page = Nokogiri::HTML(open(index_url))
     students = []
     page.css("div.student-card").each do |student|
-      name = student.css(".student-name").text
-      location = student.css(".student-location").text
-      profile_url = student.css("a").attribute("href").value
-      student_info = {:name => name,
+    name = student.css(".student-name").text
+    location = student.css(".student-location").text
+    profile_url = student.css("a").attribute("href").value
+    student_info = {:name => name,
                 :location => location,
                 :profile_url => profile_url}
-      students << student_info
+    students << student_info
       end
     students
    end
   
     	  
   def self.scrape_profile_page(profile_url)
-     page = Nokogiri::HTML(open(profile_url))
+    page = Nokogiri::HTML(open(profile_url))
     student = {}
-     container = page.css(".social-icon-container a").collect{|icon| icon.attribute("href").value}
-      container.each do |link|
+    container = page.css(".social-icon-container a").collect{|icon| icon.attribute("href").value}
+    container.each do |link|
         if link.include?("twitter")
           student[:twitter] = link
         elsif link.include?("linkedin")
